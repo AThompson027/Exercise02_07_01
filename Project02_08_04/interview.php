36967b6f4378e176b160fd75850be129e91999b2
Unknown <AThompson027@west-mec.org>
2018-11-13 12:18:33 -0800

    Table not working
    Table upload is not working for the project, but the database is

--- Project02_08_04/interview.php
+++ Project02_08_04/interview.php
@@ -1,23 +1,31 @@
 <!DOCTYPE html>
 <html lang="">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SignGuestBook.php</title>
+    <title>interviewcandidates.php</title>
     <script src="modernizr.custom.65897"></script>
 </head>
 
 <body>
-    <h1>Sign Guest Book</h1>
+    <h1>Interview Candidates</h1>
     <?php
     // global variables
     $hostname = "localhost";
     $username = "adminer";
     $password = "sense-grass-80";
-    $DBName = "guestbook";
-    $tablename = "visitors";
-    $firstName = "";
-    $lastName = "";
+    $DBName = "interview";
+    $tablename = "participants";
+    $intName = "";
+    $canName = "";
+    $position = "";
+    $DOI = "";
+    $CA = "";
+    $PA = "";
+    $CS = "";
+    $BK = "";
+    $comments = "";
     $formErrorCount = 0;
     
     // this function will connect the file to the database
@@ -34,7 +42,7 @@
     function selectDB($DBConnect, $DBName) {
         $success = mysqli_select_db($DBConnect, $DBName);
         if ($success) {
-//            echo "<p>Successfully selected the \"$DBName\" database.</p>\n";
+            echo "<p>Successfully selected the \"$DBName\" database.</p>\n";
         }
         else {
             echo "<p>Could not select the \"$DBName\" database:" . mysqli_error($DBConnect) . ", creating it.<p>\n";
@@ -44,7 +52,7 @@
                 echo "<p>Successfully created the \"$DBConnect\" database.</p>\n";
                 $success = mysqli_select_db($DBConnect, $DBName);
                 if ($success) {
-//                    echo "<p>Count not create the \"$DBName\" database.</p>\n";
+                    echo "<p>Count not create the \"$DBName\" database.</p>\n";
                 }
             } 
             else {
@@ -63,7 +71,7 @@
         if (mysqli_num_rows($result) === 0) {
             // this will create the table with a count ID and will increment with each row
             echo "The <strong>$tablename</strong> table does not exist, creating table.<br>\n";
-            $sql = "CREATE TABLE $tablename(countID SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY, lastName VARCHAR(40), firstName VARCHAR(40))";
+            $sql = "CREATE TABLE $tablename(countID SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY, intName VARCHAR(40), position VARCHAR(40), DOI DATE, canName VARCHAR(40), CA VARCHAR(40), PA VARCHAR(40), CS VARCHAR(40), BK VARCHAR(40), comments VARCHAR(40))";
             $result = mysqli_query($DBConnect, $sql);
             // if there is no result (table) then there will be an error
             if ($result === false) {
@@ -83,15 +91,29 @@
         }
         return $success;
     }
+    
     // when you press the submit button, then it will strip out and trim unnecessary characters from input to prevent bugs
     if (isset($_POST['submit'])) {
-        $firstName = stripslashes($_POST['firstName']);
-        $firstName = trim($firstName);
-        $lastName = stripslashes($_POST['lastName']);
-        $lastName = trim($lastName);
+        $intName = stripslashes($_POST['intName']);
+        $intName = trim($intName);
+        $position = stripslashes($_POST['position']);
+        $position = trim($position);
+        $canName = stripslashes($_POST['canName']);
+        $canName = trim($canName);
+        $CA = stripslashes($_POST['CA']);
+        $CA = trim($CA);
+        $PA = stripslashes($_POST['PA']);
+        $PA = trim($PA);
+        $CS = stripslashes($_POST['CS']);
+        $CS = trim($CS);
+        $BK = stripslashes($_POST['BK']);
+        $BK = trim($BK);
+        $comments = stripslashes($_POST['comments']);
+        $comments = trim($comments);
+        
         // if the inputs are empty then there will be an error
-        if (empty($firstName) || empty($lastName)) {
-            echo "<p>You must enter your first and last <strong>name</strong>.</p>\n";
+        if (empty($intName) || empty($position)|| empty($DOI) || empty($canName)  || empty($CA) || empty($PA) || empty($CS) || empty($BK) || empty($comments)) {
+            echo "<p>You must enter <strong>all</strong> of the form information.</p>\n";
             ++$formErrorCount;
         }
         if ($formErrorCount === 0) {
@@ -101,7 +123,7 @@
             if(selectDB($DBConnect, $DBName)){
                 if (createTable($DBConnect, $tablename)) {
                     echo "<p>Connection successful!</p>\n";
-                    $sql = "INSERT INTO $tablename VALUES(NULL, '$lastName', '$firstName')";
+                    $sql = "INSERT INTO $tablename VALUES(NULL, '$intName', '$position', '$DOI', '$canName', '$CA', '$PA', '$CS', '$BK', '$comments')";
                     $result = mysqli_query($DBConnect, $sql);
                     if ($result === false) {
                         // if there is no result then it will post an error
@@ -111,8 +133,15 @@
                     }
                     else {
                         echo "<h3>Thank you for signing our guest book!</h3>";
-                        $firstName = "";
-                        $lastName = "";
+                        $intName = "";
+                        $position = "";
+                        $DOI = "";
+                        $canName = "";
+                        $CA = "";
+                        $PA = "";
+                        $CS = "";
+                        $BK = "";
+                        $comments = "";
                     }
         }
             }
@@ -120,54 +149,33 @@
             mysqli_close($DBConnect);
         }
     }
-    
-    ?>
-<!--    form-->
-    <form action="SignGuestBook.php" method="post">
-        <p><strong>First Name: </strong><br>
-        <input type="text" name="firstName" value="<?php echo $firstName; ?>"></p>
-        <p><strong>Last Name: </strong><br>
-        <input type="text" name="lastName" value="<?php echo $lastName; ?>"></p>
-        <p><input type="submit" name="submit" value="Submit"></p>        
+?>
+    <!--    form-->
+
+    <form action="interview.php" method="post">
+       <p><strong>Interviewer Name: </strong><br>
+            <input type="text" name="intName" value="<?php echo $intName; ?>"></p>
+        <p><strong>Position: </strong><br>
+            <input type="text" name="position" value="<?php echo $position; ?>"></p>
+        <p><strong>Date of Interview: </strong><br>
+            <input type="date" name="date" value="<?php echo $DOI; ?>"></p>
+           <hr>
+        <p><strong>Candidate Name: </strong><br>
+            <input type="text" name="canName" value="<?php echo $canName; ?>"></p>
+        <p><strong>Communication Abilities: </strong><br>
+            <textarea name="CA" value="<?php echo $CA; ?>"></textarea></p>
+        <p><strong>Professional Appearances: </strong><br>
+            <textarea name="PA" value="<?php echo $PA; ?>"></textarea></p>
+        <p>
+            <strong>Computer Skills: </strong><br>
+            <textarea name="CS" value="<?php echo $CS; ?>"></textarea></p>
+        <p><strong>Business Knowledge: </strong><br>
+            <textarea name="BK" value="<?php echo $BK; ?>"></textarea></p>
+        <p><strong>Comments: </strong><br>
+            <textarea name="comments" value="<?php echo $comments; ?>"></textarea></p>
+        <p><input type="submit" name="submit" value="Submit"></p>
     </form>
-    <?php
-    // connects to database 
-    $DBConnect = connectToDB($hostname, $username, $password);
-    // if the database is connected then it will select the database and create the table for the visitor log
-    if ($DBConnect) {
-        if (selectDB($DBConnect, $DBName)) {
-            if (createTable($DBConnect, $tablename)) {
-//                echo "<p>Connection successful!</p>\n";
-                echo "<h2>Visitors Log</h2>";
-                $sql = "SELECT * FROM $tablename";
-                $result = mysqli_query($DBConnect, $sql);
-                // if there are no entries in the guest book then it will indicate that
-                if (mysqli_num_rows($result) == 0) {
-                    echo "<p>There are no entries in the quest book!</p>";
-                }
-                // else will create a table for the data to be inserted 
-                else {
-                    echo "<table width='60%' border='1'>";
-                    echo "<tr>";
-                    echo "<th>Visitor</th>";
-                    echo "<th>First Name</th>";
-                    echo "<th>Last Name</th>";
-                    echo "</tr>";
-                    while ($row = mysqli_fetch_row($result)) {
-                        echo "<tr>";
-                        echo "<td width='10%' style='text-align: center'>$row[0]</td>";
-                        echo "<td>$row[2]</td>";
-                        echo "<td>$row[1]</td>";
-                        echo "</tr>";
-                    }
-                    echo "</table>";
-                    // This will fetch rows from a result-set, then free the memory associated with the result
-                    mysqli_free_result($result);
-                }
-            }
-        }
-        mysqli_close($DBConnect);
-    }
-    ?>
+
 </body>
+
 </html>
